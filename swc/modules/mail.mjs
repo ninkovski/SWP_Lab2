2785bfa5de8687b364bb93aae5a62eceb7bb1b3e
nmoralesj <nmoralesj@pandero.com.pe>
2023-07-01 10:43:06 -0500

    correcciones en las importaciones

--- swc/modules/mail.mjs
+++ swc/modules/mail.mjs
@@ -4,9 +4,9 @@
 	Componente nuevo para envio de correos con https://smtpjs.com/
 	<script src="https://smtpjs.com/v3/smtp.js"></script>
 */
-import { emailParams } from '../config';
-import { validMessage } from '../params/messages';
-import { emailWelcome } from '../params/mail';
+import { emailParams } from '../config.js';
+import { validMessage } from '../params/messages.js';
+import { emailWelcome } from '../params/mail.js';
 
 document.getElementById("send-button").addEventListener("click", function (event) {
 	event.preventDefault();
